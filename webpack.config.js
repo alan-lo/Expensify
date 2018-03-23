c83d6e5f7027cece46c9a99ccefaf43eb53be4a2
Alan Lo <alan-lo@users.noreply.github.com>
2018-03-22 18:36:56 -0700

    Setup project structure

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: './src/app.js',
+  output: {
+    //must-be an absolute path to the folder
+    path: path.join(__dirname,'public') ,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.join(__dirname, "node_modules")
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader', 
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer:{
+    contentBase: path.join(__dirname,'public')
+  }
+};
