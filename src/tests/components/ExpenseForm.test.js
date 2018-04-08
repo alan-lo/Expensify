441ab31e005bd34d18e1143030384f5cce6e1b05
Alan Lo <alan-lo@users.noreply.github.com>
2018-04-08 11:32:16 -0700

    Add test spies

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
+import moment from 'moment';
 
 test('should render expense form',()=>{
   const wrapper = shallow(<ExpenseForm/>);
@@ -35,12 +36,41 @@ test('should set note on textarea change', ()=>{
 })
 
 test('should set valid amount on input change', ()=>{
-  const value = '123.2222';
+  const value = '123.22';
+  const wrapper = shallow(<ExpenseForm/>); 
+  wrapper.find('input').at(1).simulate('change',  {target: {value}}); 
+  expect(wrapper.state('amount')).toBe(value);
+})
+
+test('should not set invalid amount on input change', ()=>{
+  const value = '123.22222';
   const wrapper = shallow(<ExpenseForm/>); 
   wrapper.find('input').at(1).simulate('change',  {target: {value}}); 
   expect(wrapper.state('amount')).toBe('');
 })
 
-test('should not set invalid amount on input change', ()=>{
+test('should call onSubmit prop for valid form submission', ()=>{
+  const onsubmitSpy = jest.fn();
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onsubmitSpy}/>);
+  wrapper.find('form').simulate('submit', { preventDefault: ()=>{} });
+  expect(wrapper.state('error')).toBe('');
+  expect(onsubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  });
+});
 
+test('should set new date on date change', ()=>{
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('[onDateChange]').prop('onDateChange')(now);
+  expect(wrapper.state('createdAt')).toEqual(now);
+})
+
+test('should set new calendar focus on change', ()=>{
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('[onDateChange]').prop('onFocusChange')({focused: true});
+  expect(wrapper.state('focused')).toBe(true);
 })
