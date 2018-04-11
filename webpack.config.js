05cb4d16b4c82a1be47fcf8f691b1ce1ff8d06e9
Alan Lo <alan-lo@users.noreply.github.com>
2018-04-11 16:38:44 -0700

    Setup webpack for production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,35 @@
 const path = require('path');
 
-module.exports = {
-
-  entry: './src/app.js',
-  output: {
-    //must-be an absolute path to the folder
-    path: path.join(__dirname,'public') ,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: [
-          path.join(__dirname, "node_modules")
-        ]
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader', 
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer:{
-    contentBase: path.join(__dirname,'public'),
-    // setting historyApiFallback to true allows the server to use
-    // index.html if the requests are unknown
-    historyApiFallback: true
-  }
-};
+module.exports = (env, argv) => {
+  const isProduction = env === 'production';
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: [
+            path.join(__dirname, "node_modules")
+          ]
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
+}
